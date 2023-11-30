7e603e85fb73fa63b9751784706c073b00efb85e
roland <146352884+rolandinthout@users.noreply.github.com>
2023-11-30 12:24:30 +0100

    Add remove test at single linkedlist

--- test/nl/hva/miw/ads/linkedlists/SinglyLinkedListTest.java
+++ test/nl/hva/miw/ads/linkedlists/SinglyLinkedListTest.java
@@ -21,4 +21,42 @@ public class SinglyLinkedListTest {
         String actual = singlyLinkedList.toString();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void remove() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        System.out.println(singlyLinkedList);
+
+        singlyLinkedList.add(0, 400);
+        singlyLinkedList.add(0, 100);
+        singlyLinkedList.add(1, 200);
+        singlyLinkedList.add(2, 300);
+        singlyLinkedList.add(4, 500);
+
+        singlyLinkedList.remove( 0 );
+        String expected = "SinglyLinkedList{size=4} 200 300 400 500";
+        String actual = singlyLinkedList.toString();
+        assertEquals(expected, actual);
+
+        singlyLinkedList.remove( 3 );
+        expected = "SinglyLinkedList{size=3} 200 300 400";
+        actual = singlyLinkedList.toString();
+        assertEquals(expected, actual);
+
+        singlyLinkedList.remove( 1 );
+        expected = "SinglyLinkedList{size=2} 200 400";
+        actual = singlyLinkedList.toString();
+        assertEquals(expected, actual);
+
+        singlyLinkedList.remove( 1 );
+        expected = "SinglyLinkedList{size=1} 200";
+        actual = singlyLinkedList.toString();
+        assertEquals(expected, actual);
+
+        singlyLinkedList.remove( 0 );
+        expected = "SinglyLinkedList{size=0}";
+        actual = singlyLinkedList.toString();
+        assertEquals(expected, actual);
+    }
+
 }
