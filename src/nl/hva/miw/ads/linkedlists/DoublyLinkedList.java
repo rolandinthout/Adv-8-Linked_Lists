8933624b4186be080cc3d6b41bcc5821d1b5a6a6
roland <146352884+rolandinthout@users.noreply.github.com>
2023-11-30 15:07:41 +0100

    Added DoublyLinkedList with tests

--- src/nl/hva/miw/ads/linkedlists/DoublyLinkedList.java
+++ src/nl/hva/miw/ads/linkedlists/DoublyLinkedList.java
@@ -2,8 +2,7 @@ package nl.hva.miw.ads.linkedlists;
 
 /**
  * Doubly linked list.
- *
- * @author michel
+ * @author roland
  */
 public class DoublyLinkedList {
 
@@ -34,8 +33,12 @@ public class DoublyLinkedList {
      * @param index
      * @return
      */
-    public int get( int index ) {
-        return -1;
+    public int get(int index) {
+        Node current = head;
+        for (int count=0; count < index; count++ ) {
+            current = current.next;
+        }
+        return current.value;
     }
 
     /**
@@ -44,41 +47,85 @@ public class DoublyLinkedList {
      * @param index
      * @param value
      */
-    public void add( int index, int value ) {
+    public void add(int index, int value) {
         // Implement, create a new Node for this entry.
+        Node newNode = new Node(value);
 
-        Node n = new Node( value );
+        // Don't forget to increment the size of the list
+        size++;
 
-        // Implement the rest
+        // The simple case is if we add a new head (index == 0)
+        if (index == 0) {
+            // If head exists, it should point back to this new node
+            if (head != null) {
+                head.prev = newNode;
+            }
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
 
+        // Otherwise, follow the next-pointer as many times as necessary to get to the node at requested index,
+        // but stop one entry before that!
+        Node current = head;
+        for (int count = 0; count < index - 1; count++) {
+            current = current.next;
+        }
+
+        // Now insert the new node after the current
+        newNode.next = current.next;
+        newNode.prev = current;
+        // If there is a next node, then it should point back to this new node.
+        if (current.next != null) {
+            current.next.prev = newNode;
+        }
+        current.next = newNode;
     }
+
 
     /**
      * Remove an elmeent from the list at position index, if it exists.
      *
      * @param index
      */
-    public void remove( int index ) {
-        // Implement, remove the corresponding node from the linked list.
-    }
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
 
+        size--;
 
-    @Override
-    public String toString() {
-        StringBuffer sb = new StringBuffer();
-
-        sb.append("DoublyLinkedList{size=").append(size).append("}");
-
-        Node current = this.head;
-        while ( current != null ) {
-            sb.append(" ");
-            sb.append( current.value );
+        Node current = head;
+        for (int count=0; count < index; count++) {
             current = current.next;
         }
 
-        return sb.toString();
+        if (current.next != null) {
+            current.next.prev = current.prev;
+        }
+        if (current.prev != null) {
+            current.prev.next = current.next;
+        }
+
+        if (index == 0) {
+            head = current.next;
+        }
     }
 
+    @Override
+    public String toString() {
+        StringBuffer stringBuffer = new StringBuffer();
 
+        stringBuffer.append("DoublyLinkedList{size=").append(size).append("}");
+
+        Node current = this.head;
+        while (current != null) {
+            stringBuffer.append(" ");
+            stringBuffer.append(current.value);
+            current = current.next;
+        }
+
+        return stringBuffer.toString();
+    }
 
 }
--- test/nl/hva/miw/ads/linkedlists/DoublyLinkedListTest.java
+++ test/nl/hva/miw/ads/linkedlists/DoublyLinkedListTest.java
@@ -8,17 +8,54 @@ public class DoublyLinkedListTest {
 
     @Test
     public void add() {
-        DoublyLinkedList l = new DoublyLinkedList();
-        System.out.println(l);
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+        System.out.println(doublyLinkedList);
 
-        l.add(0, 400);
-        l.add(0, 100);
-        l.add(1, 200);
-        l.add(2, 300);
-        l.add(4, 500);
+        doublyLinkedList.add(0, 400);
+        doublyLinkedList.add(0, 100);
+        doublyLinkedList.add(1, 200);
+        doublyLinkedList.add(2, 300);
+        doublyLinkedList.add(4, 500);
 
         String expected = "DoublyLinkedList{size=5} 100 200 300 400 500";
-        String actual = l.toString();
+        String actual = doublyLinkedList.toString();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remove() {
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+        System.out.println(doublyLinkedList);
+
+        doublyLinkedList.add(0, 400);
+        doublyLinkedList.add(0, 100);
+        doublyLinkedList.add(1, 200);
+        doublyLinkedList.add(2, 300);
+        doublyLinkedList.add(4, 500);
+
+        doublyLinkedList.remove(0);
+        String expected = "DoublyLinkedList{size=4} 200 300 400 500";
+        String actual = doublyLinkedList.toString();
+        assertEquals(expected, actual);
+
+        doublyLinkedList.remove(3);
+        expected = "DoublyLinkedList{size=3} 200 300 400";
+        actual = doublyLinkedList.toString();
+        assertEquals(expected, actual);
+
+        doublyLinkedList.remove(1);
+        expected = "DoublyLinkedList{size=2} 200 400";
+        actual = doublyLinkedList.toString();
+        assertEquals(expected, actual);
+
+        doublyLinkedList.remove(1);
+        expected = "DoublyLinkedList{size=1} 200";
+        actual = doublyLinkedList.toString();
+        assertEquals(expected, actual);
+
+        doublyLinkedList.remove(0);
+        expected = "DoublyLinkedList{size=0}";
+        actual = doublyLinkedList.toString();
         assertEquals(expected, actual);
     }
 }
